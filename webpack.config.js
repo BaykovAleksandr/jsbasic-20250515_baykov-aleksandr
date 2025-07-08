940b1a8d51dd53db0486cb69487e675def85b919
Baykov Aleksandr <baykov-1988@mail.ru>
2025-07-08 23:09:27 +0300

    modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,23 +6,36 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: "./9-module/2-task/index.js",
+  entry: "./9-module/2-task/index.js", // Главный JS-файл
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "docs"),
+    path: path.resolve(__dirname, "docs"), // Выходная папка для GitHub Pages
+    publicPath: "/", // Важно для корректных путей
   },
   module: {
     rules: [
       {
-        test: /\.html$/i, // Обрабатываем все .html-файлы
+        test: /\.html$/i,
         use: "html-loader",
+      },
+      {
+        test: /\.css$/i, // Добавляем обработку CSS
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, // Обработка изображений
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[hash][ext][query]", // Сохранять в docs/assets/
+        },
       },
     ],
   },
-  mode: "production",
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./9-module/2-task/index.html",
+      template: "./9-module/2-task/index.html", // Шаблон HTML
+      filename: "index.html", // Итоговый файл в docs/
     }),
   ],
+  mode: "production",
 };
